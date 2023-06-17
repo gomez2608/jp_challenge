8eaa213fc656f7402e6149834b94a3a816a808bf
gomez2608 <s.gomeza1@uniandes.edu.co>
2023-06-16 19:37:50 -0500

    login created, html to login created - migration made and v1 working

--- jp_project/jp_project/settings.py
+++ jp_project/jp_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'soccer_ai.apps.SoccerAiConfig',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
--- jp_project/soccer_ai/views.py
+++ jp_project/soccer_ai/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+
+class HomeView(LoginRequiredMixin,View):
+    def get(self, request):
+        return HttpResponse(f"Welcome {request.user}")
